e505418788ceb6e5159e255cefa82a5e9ac8c6a6
raiseoff <108944101+raiseoff@users.noreply.github.com>
2024-11-12 13:17:47 +0300

    /start command added and polling added

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
 from config import token
+import asyncio
+import logging
+import sys
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+dp = Dispatcher()
+#Dispatcher отвечает за маршрутизацию сообщений и событий, получаемых ботом, к соответствующим обработчикам. Этот объект нужен для добавления обработчиков команд, сообщений и других событий.
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+async def main() -> None:
+    bot = Bot(token=token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
+
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+asyncio.run(main())
